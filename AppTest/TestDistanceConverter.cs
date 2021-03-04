228a5928fb86092549588a230c49212ebb936ced
Hamood Ur Rab Jaffery <72038627+hamoodjaffery@users.noreply.github.com>
2021-03-04 18:33:03 +0000

    TestScreenshotsAdded

--- AppTest/TestDistanceConverter.cs
+++ AppTest/TestDistanceConverter.cs
@@ -28,7 +28,47 @@ namespace AppTest
             converter.FromDistance = 1.0;
 
             converter.ConvertDistance();
-            Assert.AreEqual(converter.ToDistance, 1609.1);
+            Assert.AreEqual(converter.ToDistance, 1609.34);
+        }
+        public void TestFeetToMiles()
+        {
+            DistanceConverter converter = new DistanceConverter();
+            converter.FromUnit = DistanceUnits.Feet;
+            converter.ToUnit = DistanceUnits.Miles;
+            converter.FromDistance = 1.0;
+
+            converter.ConvertDistance();
+            Assert.AreEqual(converter.ToDistance, 0.000189394);
+        }
+        public void TestFeetToMeters()
+        {
+            DistanceConverter converter = new DistanceConverter();
+            converter.FromUnit = DistanceUnits.Feet;
+            converter.ToUnit = DistanceUnits.Metres;
+            converter.FromDistance = 1.0;
+
+            converter.ConvertDistance();
+            Assert.AreEqual(converter.ToDistance, 0.3048);
+        }
+        public void TestMetersToMiles()
+        {
+            DistanceConverter converter = new DistanceConverter();
+            converter.FromUnit = DistanceUnits.Metres;
+            converter.ToUnit = DistanceUnits.Miles;
+            converter.FromDistance = 1.0;
+
+            converter.ConvertDistance();
+            Assert.AreEqual(converter.ToDistance, 0.000621371);
+        }
+        public void TestMetersToFeet()
+        {
+            DistanceConverter converter = new DistanceConverter();
+            converter.FromUnit = DistanceUnits.Metres;
+            converter.ToUnit = DistanceUnits.Feet;
+            converter.FromDistance = 1.0;
+
+            converter.ConvertDistance();
+            Assert.AreEqual(converter.ToDistance, 3.28);
         }
     }
 }
